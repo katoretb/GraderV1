031610c94bd01af01c3e534232c4e328176a7c72
katoretb <katoretv@gmail.com>
2024-11-19 02:43:28 +0700

    selfhost

--- frontend/src/src/index.js
+++ frontend/src/src/index.js
@@ -5,6 +5,7 @@ import App from './components/App';
 import reportWebVitals from './components/reportWebVitals';
 import { BrowserRouter as Router } from 'react-router-dom'
 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
